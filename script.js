1a2bb58af4deefa5110a9fa7b3b8e0c108bcc21b
Glitch (glitch-hello-website) <none>
2021-08-14 05:41:34 +0000

    🎬⛺️ Checkpoint ./script.js:34469399/384

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 let data = [
-  {type:name:'Bk W sch', quantity: 8}, 
-  {name: 'Neil W mug', quantity:20, notes:'4 to display table'}, 
-  {name: 'GY seal crew', quantity: {S: 3, M: 2, L:1}}, 
-  {name: '47 Wt retro dog tee', quantity:{S:2, M:2}, notes:'folded'}
+  {type:'general', name:'Bk W sch', quantity: 8}, 
+  {type: 'general', name: 'Neil W mug', quantity:20, notes:'4 to display table'}, 
+  {type: 'clothing', name: 'GY seal crew', quantity: {S: 3, M: 2, L:1}}, 
+  {type: 'clothing', name: '47 Wt retro dog tee', quantity:{S:2, M:2}, notes:'folded'}
 ];
 
-function getApparrelLine(line){
-  
+function getListItem(){
+  const l = document.createElement('li');
+  l.classList.add('list-group-item');
+  return l;
 }
 
-function getGeneralLine(line)
+function getClothingLine(line){
+  const l = getListItem();
+  
+  return l;
+}
+
+function getGeneralLine(line){
+  
+}
