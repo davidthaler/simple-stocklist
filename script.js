272a62ff27a0d1b19625246d52d5ecee13bf6e9c
Glitch (glitch-hello-website) <none>
2021-08-14 05:30:33 +0000

    ☃️🛣 Checkpoint ./script.js:34469399/374

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let data = [
+  {type:name:'Bk W sch', quantity: 8}, 
+  {name: 'Neil W mug', quantity:20, notes:'4 to display table'}, 
+  {name: 'GY seal crew', quantity: {S: 3, M: 2, L:1}}, 
+  {name: '47 Wt retro dog tee', quantity:{S:2, M:2}, notes:'folded'}
+];
 
+function getApparrelLine(line){
+  
+}
+
+function getGeneralLine(line)
