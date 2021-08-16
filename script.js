2fb14b51f8d33b185de089fdbfae340711e84055
Glitch (glitch-hello-website) <none>
2021-08-16 04:40:31 +0000

    🐡🌊 Checkpoint ./script.js:34469399/882

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let data = [
-  {type:'general', name:'Bk W sch', count: 8}, 
-  {type: 'general', name: 'Neil W mug', count:20, notes:'4 to display table'}, 
-  {type: 'clothing', name: 'GY seal crew', count: {S: 3, M: 2, L:1}}, 
-  {type: 'clothing', name: '47 Wt retro dog tee', count:{S:2, M:2}, notes:'folded'}
+  {type:'general', description:'Bk W sch', count: 8}, 
+  {type: 'general', description: 'Neil W mug', count:20, notes:'4 to display table'}, 
+  {type: 'clothing', description: 'GY seal crew', counts: {S: 3, M: 2, L:1}}, 
+  {type: 'clothing', description: '47 Wt retro dog tee', counts:{S:2, M:2}, notes:'folded'}
 ];
 
 function getListItem(){
-  const l = document.createElement('li');
+  const l = 
   l.classList.add('list-group-item');
   return l;
 }
@@ -18,21 +18,16 @@ function getClothingLine(line){
 }
 
 function getGeneralLine(line){
-  const l = getListItem();
-  if(line.name && line.count){
-    const p1 = document.createElement('p');
-    p1.textContent = `name: ${line.name}`;
-    l.appendChild(p1);
-    const p2 = document.createElement('p');
-    p2.textContent = `count: ${line.count}`;
-    l.appendChild(p2);
-  }else{
-    return l;
+  const l = document.createElement('li');
+  l.classList.add('list-group-item');
+  if(!line || !line.description) return;
+  let html = `<h5>${line.description}</h5>`;
+  if(line.count){
+    html += `<p>${line.count}</p>`;
   }
   if(line.notes){
-    const pNote = document.createElement('p');
-    pNote.textContent = `name: ${line.notes}`;
-    l.appendChild(pNote);
+    html += `<p class="text-muted">${line.notes}</p>`
   }
+  l.innerHTML = html;
   return l;
 }
