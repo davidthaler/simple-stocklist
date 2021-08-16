7d638f5fa4d44f5d07e3f9e196d3d1179fd76c5e
Glitch (glitch-hello-website) <none>
2021-08-16 04:51:39 +0000

    🐔🏰 Checkpoint ./script.js:34469399/2070 ./index.html:34469399/13522

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 let data = [
-  {type:'general', description:'Bk W sch', count: 8}, 
-  {type: 'general', description: 'Neil W mug', count:20, notes:'4 to display table'}, 
-  {type: 'clothing', description: 'GY seal crew', counts: {S: 3, M: 2, L:1}}, 
-  {type: 'clothing', description: '47 Wt retro dog tee', counts:{S:2, M:2}, notes:'folded'}
+  { type: "unsized", description: "Bk W sch", count: 8 },
+  {
+    type: "unsized",
+    description: "Neil W mug",
+    count: 20,
+    notes: "4 to display table"
+  },
+  { type: "sized", description: "GY seal crew", counts: { S: 3, M: 2, L: 1 } },
+  {
+    type: "sized",
+    description: "47 Wt retro dog tee",
+    counts: { S: 2, M: 2 },
+    notes: "folded"
+  }
 ];
 
-function getListItem(){
-  const l = 
-  l.classList.add('list-group-item');
-  return l;
-}
-
-function getClothingLine(line){
-  const l = getListItem();
-  
-  return l;
-}
-
-function getGeneralLine(line){
-  const l = document.createElement('li');
-  l.classList.add('list-group-item');
-  if(!line || !line.description) return;
+function getLine(line) {
+  const l = document.createElement("li");
+  l.classList.add("list-group-item");
+  if (!line || !line.description) return;
   let html = `<h5>${line.description}</h5>`;
-  if(line.count){
-    html += `<p>${line.count}</p>`;
+  if (line.type && line.type === "unsized") {
+    if (line.count) {
+      html += `<p>${line.count}</p>`;
+    }
+  }else if(line.type && line.type==='sized'){
+    if(line.counts){
+      html += 'counts...';
+    }
   }
-  if(line.notes){
-    html += `<p class="text-muted">${line.notes}</p>`
+  if (line.notes) {
+    html += `<p class="text-muted">${line.notes}</p>`;
   }
   l.innerHTML = html;
   return l;
